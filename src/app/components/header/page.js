0247fea6c7c199f0d4b5319d9257796d87d8f779
Basit-Bashir <wbhat56@gmail.com>
2024-01-23 19:29:38 +0530

    updated navbar

--- src/app/components/header/page.js
+++ src/app/components/header/page.js
@@ -4,10 +4,10 @@ export default function Header() {
   return (
     <div className="p-6 mx-auto shadow-md shadow-green-200">
       <div className="flex items-center font-semibold tracking-wider justify-between text-lg">
-        <Link href="" className="font-bold uppercase text-2xl">
+        <Link href="" className="font-bold uppercase text-4xl md:text-2xl">
           North-Pizza
         </Link>
-        <nav className="flex text-xl gap-8 items-center">
+        <nav className="hidden md:block md:flex text-xl gap-8 items-center">
           <Link
             href=""
             className=" hover:bg-gradient-to-b from-yellow-500 to-orange-500 p-3 transition duration-300 hover:text-white rounded-xl"
